e502a8e39bf5e02568bbee05212927dc40f545a4
weiensong <1243569769@qq.com>
2023-08-19 19:15:54 +0800

    fix: recommit

--- src/constants.py
+++ src/constants.py
@@ -3,20 +3,12 @@ from enum import Enum, unique, IntEnum
 
 @unique
 class API(Enum):
-    PROVINCE_COUNT = 'province_count'  # 各个省份包含大学
-    DUAL_COUNT = 'dual_class_count'  # 各省份包含双一流数量
-    TYPE_COUNT = 'type_count'  # 学校类别统计
-    SPECIAL_COUNT = 'spacial_name_count'  # 所有专业统计
-    SCORE_PROVINCE = 'score_province'  # 招生统计
-    BIG_DATA_COUNT = 'big_data_count'  # 不同大数据专业统计
-    BIG_DATA_PROVINCE_COUNT = 'big_data_province_count'  # 含大数据专业的省份内大学统计
-    BIG_DATA_TYPE_COUNT = 'big_data_type_count'  # 大数据本科专科统计
-    BIG_DATA_LEVEL2_COUNT = 'big_data_level2_count'  # 大数据二级类目统计
-    BIG_DATA_LEVEL3_COUNT = 'big_data_level3_count'  # 大数据一级类目统计
-    BIG_DATA_IN_DUAL = 'big_data_in_dual'  # 大数据在双一流占比
-    BIG_DATA_IN_NULL = 'big_data_in_null'  # 大数据在普通院校占比
-    ARTIFICIAL_INTELLIGENCE_IN_DUAL = 'artificial_intelligence_in_dual'  # 人工智能在双一流占比
-    ARTIFICIAL_INTELLIGENCE_IN_NULL = 'artificial_intelligence_in_null'  # 人工智能在普通院校占比
+    ERROR = '/'  # 未知
+    PROVINCE_COUNT = '/province_count'  # 各个省份包含大学
+    DUAL_COUNT = '/dual_class_count'  # 各省份包含双一流数量
+    TYPE_COUNT = '/type_count'  # 学校类别统计
+    SPECIAL_COUNT = '/spacial_name_count'  # 所有专业统计
+    SCORE_PROVINCE = '/score_province'  # 招生统计
 
 
 class MyJson(Enum):
@@ -25,52 +17,43 @@ class MyJson(Enum):
     TYPE_COUNT = 'result_json/type_count.json'  # 学校类别统计
     SPECIAL_COUNT = 'result_json/spacial_name_count.json'  # 所有专业统计
     SCORE_PROVINCE = 'result_json/score_province.json'  # 招生统计
-    BIG_DATA_COUNT = 'result_json/big_data_count.json'  # 不同大数据专业统计
-    BIG_DATA_PROVINCE_COUNT = 'result_json/big_data_province_count.json'  # 含大数据专业的省份内大学统计
-    BIG_DATA_TYPE_COUNT = 'result_json/big_data_type_count.json'  # 大数据本科专科统计
-    BIG_DATA_LEVEL2_COUNT = 'result_json/big_data_level2_count.json'  # 大数据二级类目统计
-    BIG_DATA_LEVEL3_COUNT = 'result_json/big_data_level3_count.json'  # 大数据一级类目统计
-    BIG_DATA_IN_DUAL = 'result_json/big_data_in_dual.json'  # 大数据在双一流占比
-    BIG_DATA_IN_NULL = 'result_json/big_data_in_null.json'  # 大数据在普通院校占比
-    ARTIFICIAL_INTELLIGENCE_IN_DUAL = 'result_json/artificial_intelligence_in_dual.json'  # 人工智能在双一流占比
-    ARTIFICIAL_INTELLIGENCE_IN_NULL = 'result_json/artificial_intelligence_in_null.json'  # 人工智能在普通院校占比
 
 
 @unique
 class Province(Enum):
-    JIANGSU = "江苏省"
-    GUANGDONG = "广东省"
-    HENAN = "河南省"
-    SHANDONG = "山东省"
-    SICHUAN = "四川省"
-    HUBEI = "湖北省"
-    HUNAN = "湖南省"
-    ANHUI = "安徽省"
-    HEBEI = "河北省"
-    ZHEJIANG = "浙江省"
-    LIAONING = "辽宁省"
-    JIANGXI = "江西省"
-    SHANNXI = "陕西省"
-    BEIJING = "北京市"
-    FUJIAN = "福建省"
-    YUNNAN = "云南省"
-    GUANGXI = "广西壮族自治区"
-    SHANXI = "山西省"
-    HEILONGJIANG = "黑龙江省"
-    CHONGQING = "重庆市"
-    GUIZHOU = "贵州省"
-    JILIN = "吉林省"
-    SHANGHAI = "上海市"
-    TIANJIN = "天津市"
-    XINJIANG = "新疆维吾尔自治区"
-    NEIMENGGU = "内蒙古自治区"
-    GANSU = "甘肃省"
-    HAINAN = "海南省"
-    NINGXIA = "宁夏回族自治区"
-    XIANGGANG = "香港特别行政区"
-    QINGHAI = "青海省"
-    XIZANG = "西藏自治区"
-    AOMEN = "澳门特别行政区"
+    JIANGSU = "江苏"
+    GUANGDONG = "广东"
+    HENAN = "河南"
+    SHANDONG = "山东"
+    SICHUAN = "四川"
+    HUBEI = "湖北"
+    HUNAN = "湖南"
+    ANHUI = "安徽"
+    HEBEI = "河北"
+    ZHEJIANG = "浙江"
+    LIAONING = "辽宁"
+    JIANGXI = "江西"
+    SHANNXI = "陕西"
+    BEIJING = "北京"
+    FUJIAN = "福建"
+    YUNNAN = "云南"
+    GUANGXI = "广西"
+    SHANXI = "山西"
+    HEILONGJIANG = "黑龙江"
+    CHONGQING = "重庆"
+    GUIZHOU = "贵州"
+    JILIN = "吉林"
+    SHANGHAI = "上海"
+    TIANJIN = "天津"
+    XINJIANG = "新疆"
+    NEIMENGGU = "内蒙古"
+    GANSU = "甘肃"
+    HAINAN = "海南"
+    NINGXIA = "宁夏"
+    XIANGGANG = "香港"
+    QINGHAI = "青海"
+    XIZANG = "西藏"
+    AOMEN = "澳门"
 
 
 @unique
