233adc093bf879b122fdc3eeec8d82e0058c93f5
Tououououer <61647484+weiensong@users.noreply.github.com>
2023-08-31 09:11:04 +0000

    refactor: getData as the depend

--- src/api.py
+++ src/api.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from starlette.responses import JSONResponse
 from src.getdata import GetData
 from util.tools import log_t
 
 app = FastAPI()
-data = GetData()
+
+
+@app.get('/api/{item}')
+async def start_api(get_data: GetData = Depends(GetData)):
+    result = get_data.api_select(item)
+    return JSONResponse(result)
 
 
 @app.middleware('http')
@@ -13,10 +18,3 @@ async def log_requests(requests, call_next):
     str_result = f'{str(requests.client)} [{requests.method} {str(response.status_code)}] {str(requests.url)}'
     log_t(str_result)
     return response
-
-
-@app.get('/api/{item}')
-async def start_api(item: str):
-    result = data.api_select(item)
-    return JSONResponse(result)
-
