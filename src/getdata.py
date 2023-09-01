185efdecb1fffacdb154b0647e8aef7faa106466
weiensong <1243569769@qq.com>
2023-09-01 22:49:09 +0800

    refactor: class Tools

--- src/getdata.py
+++ src/getdata.py
@@ -3,7 +3,7 @@ from typing import Optional
 from constants import MyJson, API, Province
 from sql_master import SqlMaster
 
-from tools import turn_to_dict_of_list, save_json, exist_json, both_count, hava_count
+from tools import Tools, exist_json
 
 
 class GetData:
@@ -50,17 +50,17 @@ class GetData:
     def get_province(self) -> json:
         province_data = self.sql.execute_sql('sql/province_count.sql')
         province_data_count = Province.province_mapping(dict(province_data), 1)
-        listResult = turn_to_dict_of_list(province_data_count)
-        save_json(str(MyJson.PROVINCE_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(province_data_count)
+        Tools.save_json(str(MyJson.PROVINCE_COUNT.value), listResult)
         return listResult
 
     @property
     @exist_json(MyJson.DUAL_COUNT.value)
     def get_dual_class_name(self) -> json:
         dual_data = self.sql.execute_sql('sql/dual_class_count.sql')
-        provinceDict = both_count(dual_data)
-        listResult = turn_to_dict_of_list(provinceDict)
-        save_json(str(MyJson.DUAL_COUNT.value), listResult)
+        provinceDict = Tools.both_count(dual_data)
+        listResult = Tools.turn_to_dict_of_list(provinceDict)
+        Tools.save_json(str(MyJson.DUAL_COUNT.value), listResult)
         return listResult
 
     @property
@@ -68,8 +68,8 @@ class GetData:
     def get_type_name(self) -> json:
         type_name_data = self.sql.execute_sql('sql/type_count.sql')
         type_nameDict = dict(type_name_data)
-        listResult = turn_to_dict_of_list(type_nameDict)
-        save_json(str(MyJson.TYPE_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(type_nameDict)
+        Tools.save_json(str(MyJson.TYPE_COUNT.value), listResult)
         return listResult
 
     @property
@@ -77,8 +77,8 @@ class GetData:
     def get_special_count(self) -> json:
         special_names = self.sql.execute_sql('sql/spacial_name_count.sql')
         special_nameDict = dict(special_names)
-        listResult = turn_to_dict_of_list(special_nameDict)
-        save_json(str(MyJson.SPECIAL_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(special_nameDict)
+        Tools.save_json(str(MyJson.SPECIAL_COUNT.value), listResult)
         return listResult
 
     @property
@@ -86,17 +86,17 @@ class GetData:
     def get_score_count(self) -> json:
         score_province = self.sql.execute_sql('sql/score_province.sql')
         score_provinceDict = Province.province_mapping(dict(score_province))
-        listResult = turn_to_dict_of_list(score_provinceDict)
-        save_json(str(MyJson.SCORE_PROVINCE.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(score_provinceDict)
+        Tools.save_json(str(MyJson.SCORE_PROVINCE.value), listResult)
         return listResult
 
     @property
     @exist_json(MyJson.BIG_DATA_COUNT.value)
     def get_big_data_count(self) -> json:
         big_data = self.sql.execute_sql('sql/big_data_count.sql')
-        specialDict = both_count(big_data)
-        listResult = turn_to_dict_of_list(specialDict)
-        save_json(str(MyJson.BIG_DATA_COUNT.value), listResult)
+        specialDict = Tools.both_count(big_data)
+        listResult = Tools.turn_to_dict_of_list(specialDict)
+        Tools.save_json(str(MyJson.BIG_DATA_COUNT.value), listResult)
         return listResult
 
     @property
@@ -104,8 +104,8 @@ class GetData:
     def get_big_data_province_count(self) -> json:
         big_data_province_count = self.sql.execute_sql('sql/big_data_province_count.sql')
         big_data_provinceDict = Province.province_mapping(dict(big_data_province_count))
-        listResult = turn_to_dict_of_list(big_data_provinceDict)
-        save_json(str(MyJson.BIG_DATA_PROVINCE_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(big_data_provinceDict)
+        Tools.save_json(str(MyJson.BIG_DATA_PROVINCE_COUNT.value), listResult)
         return listResult
 
     @property
@@ -113,8 +113,8 @@ class GetData:
     def get_big_data_type_count(self) -> json:
         big_data_type_count = self.sql.execute_sql('sql/big_data_type_count.sql')
         big_data_typeDict = dict(big_data_type_count)
-        listResult = turn_to_dict_of_list(big_data_typeDict)
-        save_json(str(MyJson.BIG_DATA_TYPE_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(big_data_typeDict)
+        Tools.save_json(str(MyJson.BIG_DATA_TYPE_COUNT.value), listResult)
         return listResult
 
     @property
@@ -122,8 +122,8 @@ class GetData:
     def get_big_data_level2_count(self) -> json:
         big_data_level2_count = self.sql.execute_sql('sql/big_data_level2_count.sql')
         big_data_typeDict = dict(big_data_level2_count)
-        listResult = turn_to_dict_of_list(big_data_typeDict)
-        save_json(str(MyJson.BIG_DATA_LEVEL2_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(big_data_typeDict)
+        Tools.save_json(str(MyJson.BIG_DATA_LEVEL2_COUNT.value), listResult)
         return listResult
 
     @property
@@ -131,8 +131,8 @@ class GetData:
     def get_big_data_level3_count(self) -> json:
         big_data_level3_count = self.sql.execute_sql('sql/big_data_level3_count.sql')
         big_data_typeDict = dict(big_data_level3_count)
-        listResult = turn_to_dict_of_list(big_data_typeDict)
-        save_json(str(MyJson.BIG_DATA_LEVEL3_COUNT.value), listResult)
+        listResult = Tools.turn_to_dict_of_list(big_data_typeDict)
+        Tools.save_json(str(MyJson.BIG_DATA_LEVEL3_COUNT.value), listResult)
         return listResult
 
     @property
@@ -140,8 +140,8 @@ class GetData:
     def get_big_data_in_dual(self) -> json:
         school_dual = self.sql.get_something_count('sql/dual.sql')
         big_data_count_temp = self.sql.get_something_count('sql/big_data.sql')
-        result = hava_count(school_dual, big_data_count_temp)
-        save_json(str(MyJson.BIG_DATA_IN_DUAL.value), result)
+        result = Tools.hava_count(school_dual, big_data_count_temp)
+        Tools.save_json(str(MyJson.BIG_DATA_IN_DUAL.value), result)
         return result
 
     @property
@@ -149,8 +149,8 @@ class GetData:
     def get_big_data_in_null(self) -> json:
         school_dual = self.sql.get_something_count('sql/null.sql')
         big_data_count_temp = self.sql.get_something_count('sql/big_data.sql')
-        result = hava_count(school_dual, big_data_count_temp)
-        save_json(str(MyJson.BIG_DATA_IN_NULL.value), result)
+        result = Tools.hava_count(school_dual, big_data_count_temp)
+        Tools.save_json(str(MyJson.BIG_DATA_IN_NULL.value), result)
         return result
 
     @property
@@ -158,8 +158,8 @@ class GetData:
     def get_artificial_intelligence_in_dual(self) -> json:
         school_dual = self.sql.get_something_count('sql/dual.sql')
         count_temp = self.sql.get_something_count('sql/artificial_intelligence.sql')
-        result = hava_count(school_dual, count_temp)
-        save_json(str(MyJson.ARTIFICIAL_INTELLIGENCE_IN_DUAL.value), result)
+        result = Tools.hava_count(school_dual, count_temp)
+        Tools.save_json(str(MyJson.ARTIFICIAL_INTELLIGENCE_IN_DUAL.value), result)
         return result
 
     @property
@@ -167,6 +167,6 @@ class GetData:
     def get_artificial_intelligence_in_null(self) -> json:
         school_dual = self.sql.get_something_count('sql/null.sql')
         count_temp = self.sql.get_something_count('sql/artificial_intelligence.sql')
-        result = hava_count(school_dual, count_temp)
-        save_json(str(MyJson.ARTIFICIAL_INTELLIGENCE_IN_NULL.value), result)
+        result = Tools.hava_count(school_dual, count_temp)
+        Tools.save_json(str(MyJson.ARTIFICIAL_INTELLIGENCE_IN_NULL.value), result)
         return result
--- src/tools.py
+++ src/tools.py
@@ -4,6 +4,22 @@ import json
 import random
 
 
+def exist_json(json_path: object):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            listResult = Tools.get_json(str(json_path))
+            if listResult and random.choice([0, 1]):
+                log_t(f'is update: 0')
+                return listResult
+            else:
+                log_t(f'is update: 1')
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
 def log_t(*args):
     msg = ','.join(args)
     logger = logging.getLogger('api')
@@ -21,65 +37,51 @@ def log_t(*args):
     logger.removeHandler(console_handler)
 
 
-def get_json(name: str) -> json:
-    if os.path.exists(name):
-        log_t(f'find it: {name}')
-        with open(name, 'r', encoding='utf-8') as json_f:
-            jsonResult = json.load(json_f)
-        return jsonResult
-    else:
-        return []
+class Tools:
+    @staticmethod
+    def get_json(name: str) -> json:
+        if os.path.exists(name):
+            log_t(f'find it: {name}')
+            with open(name, 'r', encoding='utf-8') as json_f:
+                jsonResult = json.load(json_f)
+            return jsonResult
+        else:
+            return []
 
+    @staticmethod
+    def save_json(name: str, _list: []):
+        if not os.path.exists(name):
+            with open(name, 'w', encoding='utf-8') as json_f:
+                json_f.write(json.dumps(_list))
+                log_t(f'writing json to: {name}')
 
-def save_json(name: str, _list: []):
-    if not os.path.exists(name):
-        with open(name, 'w', encoding='utf-8') as json_f:
-            json_f.write(json.dumps(_list))
-            log_t(f'writing json to: {name}')
+    @staticmethod
+    def turn_to_dict_of_list(dict_args) -> json:
+        result = []
+        for i in range(0, len(dict_args)):
+            itemDict = dict_args.popitem()
+            item = {'province': itemDict[0], 'detail': itemDict[1]}
+            result.append(item)
+        return result
 
+    @staticmethod
+    def both_count(execute_sql_result: tuple) -> dict:
+        result = {}
+        for item in execute_sql_result:
+            special_name = item[0]
+            if special_name in result:
+                result[item[0]] += 1
+            elif special_name not in result:
+                result[item[0]] = 1
+        return result
 
-def turn_to_dict_of_list(dict_args) -> json:
-    result = []
-    for i in range(0, len(dict_args)):
-        itemDict = dict_args.popitem()
-        item = {'province': itemDict[0], 'detail': itemDict[1]}
-        result.append(item)
-    return result
-
-
-def exist_json(json_path: object):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            listResult = get_json(str(json_path))
-            if listResult and random.choice([0, 1]):
-                log_t(f'is update: 0')
-                return listResult
-            else:
-                log_t(f'is update: 1')
-                return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-def both_count(execute_sql_result: tuple) -> dict:
-    result = {}
-    for item in execute_sql_result:
-        special_name = item[0]
-        if special_name in result:
-            result[item[0]] += 1
-        elif special_name not in result:
-            result[item[0]] = 1
-    return result
-
-
-def hava_count(hava: list, not_have: list) -> list:
-    count_dual = 0
-    for temp in not_have:
-        if temp in hava:
-            count_dual += 1
-    other = len(hava) - count_dual
-    result = [{'name': '拥有', 'count': count_dual},
-              {'name': '未拥有', 'count': other}]
-    return result
+    @staticmethod
+    def hava_count(hava: list, not_have: list) -> list:
+        count_dual = 0
+        for temp in not_have:
+            if temp in hava:
+                count_dual += 1
+        other = len(hava) - count_dual
+        result = [{'name': '拥有', 'count': count_dual},
+                  {'name': '未拥有', 'count': other}]
+        return result
