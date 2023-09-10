b162ccc3c8bd718e2fa2787f3f8831dd51212b2a
weiensong <1243569769@qq.com>
2023-09-10 15:51:53 +0800

    fix: run in docker

--- src/run_in_docker.py
+++ src/run_in_docker.py
@@ -6,24 +6,28 @@
 import docker
 
 
-def run_image(client, command):
-    if client is not None:
-        image = search_image(client)
-        if image is None:
-            client.pull('', stream=True)
-            image = search_image(client)
-        client.containers.run(image, command, ports={'2518/tcp': 2518})
+class FishPond:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.run_command = 'python -c local_runner.py xxx.yaml'
 
+    def run_image(self):
+        if self.client is not None:
+            image = self.search_image
+            if image is None:
+                self.client.pull('', stream=True)
+                image = self.search_image
+            self.client.containers.run(image, self.run_command, ports={'2518/tcp': 2518})
 
-def search_image(client):
-    local_images = client.images()
-    for local_image in local_images:
-        if 'xxx' in local_image:
-            return local_image
-    return None
+    @property
+    def search_image(self):
+        local_images = self.client.images()
+        for local_image in local_images:
+            if 'xxx' in local_image:
+                return local_image
+        return None
 
 
 if __name__ == '__main__':
-    command = 'python -c local_runner.py xxx.yaml'
-    client = docker.from_env()
-    run_image(client)
+    fish_pond = FishPond()
+    fish_pond.run_image()
