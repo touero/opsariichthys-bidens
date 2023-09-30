ecf98a43d8f8857f466e77bb402748ec15334526
1243569769@qq.com <1243569769@qq.com>
2023-09-30 18:16:14 +0800

    refactor: rename and add a catch exception

--- src/api.py
+++ src/api.py
@@ -5,16 +5,18 @@ from tools import log_t
 
 app = FastAPI()
 
+try:
+    @app.get('/api/{item}')
+    async def start_api(item, sql=Depends(SqlMaster), get_data=Depends(GetData)):
+        get_data.sql_init(sql=sql)
+        return get_data.api_select(item)
 
-@app.get('/api/{item}')
-async def start_api(item, sql=Depends(SqlMaster), get_data=Depends(GetData)):
-    get_data.sql_init(sql=sql)
-    return get_data.api_select(item)
 
-
-@app.middleware('http')
-async def log_requests(requests, call_next):
-    response = await call_next(requests)
-    str_result = f'{str(requests.client)} [{requests.method} {str(response.status_code)}] {str(requests.url)}'
-    log_t(str_result)
-    return response
+    @app.middleware('http')
+    async def log_requests(requests, call_next):
+        response = await call_next(requests)
+        str_result = f'{str(requests.client)} [{requests.method} {str(response.status_code)}] {str(requests.url)}'
+        log_t(str_result)
+        return response
+except Exception as e:
+    log_t(str(e))
--- src/constants.py
+++ src/constants.py
@@ -77,80 +77,80 @@ class Province(Enum):
     AOMEN = "澳门特别行政区"
 
     @staticmethod
-    def province_mapping(dictObject: dict, args=0) -> dict:
+    def province_mapping(dict_object: dict, args=0) -> dict:
         province = None
         if args == 0:
             province = ProvinceID
         elif args == 1:
             province = Province
-        for item in list(dictObject):
+        for item in list(dict_object):
             if item in province.JIANGSU.value:
-                dictObject[Province.JIANGSU.value] = dictObject.pop(item)
+                dict_object[Province.JIANGSU.value] = dict_object.pop(item)
             elif item in province.GUANGDONG.value:
-                dictObject[Province.GUANGDONG.value] = dictObject.pop(item)
+                dict_object[Province.GUANGDONG.value] = dict_object.pop(item)
             elif item in province.HENAN.value:
-                dictObject[Province.HENAN.value] = dictObject.pop(item)
+                dict_object[Province.HENAN.value] = dict_object.pop(item)
             elif item in province.SHANDONG.value:
-                dictObject[Province.SHANDONG.value] = dictObject.pop(item)
+                dict_object[Province.SHANDONG.value] = dict_object.pop(item)
             elif item in province.SICHUAN.value:
-                dictObject[Province.SICHUAN.value] = dictObject.pop(item)
+                dict_object[Province.SICHUAN.value] = dict_object.pop(item)
             elif item in province.HUBEI.value:
-                dictObject[Province.HUBEI.value] = dictObject.pop(item)
+                dict_object[Province.HUBEI.value] = dict_object.pop(item)
             elif item in province.HUNAN.value:
-                dictObject[Province.HUNAN.value] = dictObject.pop(item)
+                dict_object[Province.HUNAN.value] = dict_object.pop(item)
             elif item in province.ANHUI.value:
-                dictObject[Province.ANHUI.value] = dictObject.pop(item)
+                dict_object[Province.ANHUI.value] = dict_object.pop(item)
             elif item in province.HEBEI.value:
-                dictObject[Province.HEBEI.value] = dictObject.pop(item)
+                dict_object[Province.HEBEI.value] = dict_object.pop(item)
             elif item in province.ZHEJIANG.value:
-                dictObject[Province.ZHEJIANG.value] = dictObject.pop(item)
+                dict_object[Province.ZHEJIANG.value] = dict_object.pop(item)
             elif item in province.LIAONING.value:
-                dictObject[Province.LIAONING.value] = dictObject.pop(item)
+                dict_object[Province.LIAONING.value] = dict_object.pop(item)
             elif item in province.JIANGXI.value:
-                dictObject[Province.JIANGXI.value] = dictObject.pop(item)
+                dict_object[Province.JIANGXI.value] = dict_object.pop(item)
             elif item in province.SHANNXI.value:
-                dictObject[Province.SHANNXI.value] = dictObject.pop(item)
+                dict_object[Province.SHANNXI.value] = dict_object.pop(item)
             elif item in province.BEIJING.value:
-                dictObject[Province.BEIJING.value] = dictObject.pop(item)
+                dict_object[Province.BEIJING.value] = dict_object.pop(item)
             elif item in province.FUJIAN.value:
-                dictObject[Province.FUJIAN.value] = dictObject.pop(item)
+                dict_object[Province.FUJIAN.value] = dict_object.pop(item)
             elif item in province.YUNNAN.value:
-                dictObject[Province.YUNNAN.value] = dictObject.pop(item)
+                dict_object[Province.YUNNAN.value] = dict_object.pop(item)
             elif item in province.GUANGXI.value:
-                dictObject[Province.GUANGXI.value] = dictObject.pop(item)
+                dict_object[Province.GUANGXI.value] = dict_object.pop(item)
             elif item in province.SHANXI.value:
-                dictObject[Province.SHANXI.value] = dictObject.pop(item)
+                dict_object[Province.SHANXI.value] = dict_object.pop(item)
             elif item in province.HEILONGJIANG.value:
-                dictObject[Province.HEILONGJIANG.value] = dictObject.pop(item)
+                dict_object[Province.HEILONGJIANG.value] = dict_object.pop(item)
             elif item in province.CHONGQING.value:
-                dictObject[Province.CHONGQING.value] = dictObject.pop(item)
+                dict_object[Province.CHONGQING.value] = dict_object.pop(item)
             elif item in province.GUIZHOU.value:
-                dictObject[Province.GUIZHOU.value] = dictObject.pop(item)
+                dict_object[Province.GUIZHOU.value] = dict_object.pop(item)
             elif item in province.JILIN.value:
-                dictObject[Province.JILIN.value] = dictObject.pop(item)
+                dict_object[Province.JILIN.value] = dict_object.pop(item)
             elif item in province.SHANGHAI.value:
-                dictObject[Province.SHANGHAI.value] = dictObject.pop(item)
+                dict_object[Province.SHANGHAI.value] = dict_object.pop(item)
             elif item in province.TIANJIN.value:
-                dictObject[Province.TIANJIN.value] = dictObject.pop(item)
+                dict_object[Province.TIANJIN.value] = dict_object.pop(item)
             elif item in province.XINJIANG.value:
-                dictObject[Province.XINJIANG.value] = dictObject.pop(item)
+                dict_object[Province.XINJIANG.value] = dict_object.pop(item)
             elif item in province.NEIMENGGU.value:
-                dictObject[Province.NEIMENGGU.value] = dictObject.pop(item)
+                dict_object[Province.NEIMENGGU.value] = dict_object.pop(item)
             elif item in province.GANSU.value:
-                dictObject[Province.GANSU.value] = dictObject.pop(item)
+                dict_object[Province.GANSU.value] = dict_object.pop(item)
             elif item in province.HAINAN.value:
-                dictObject[Province.HAINAN.value] = dictObject.pop(item)
+                dict_object[Province.HAINAN.value] = dict_object.pop(item)
             elif item in province.NINGXIA.value:
-                dictObject[Province.NINGXIA.value] = dictObject.pop(item)
+                dict_object[Province.NINGXIA.value] = dict_object.pop(item)
             elif item in province.XIANGGANG.value:
-                dictObject[Province.XIANGGANG.value] = dictObject.pop(item)
+                dict_object[Province.XIANGGANG.value] = dict_object.pop(item)
             elif item in province.QINGHAI.value:
-                dictObject[Province.QINGHAI.value] = dictObject.pop(item)
+                dict_object[Province.QINGHAI.value] = dict_object.pop(item)
             elif item in province.XIZANG.value:
-                dictObject[Province.XIZANG.value] = dictObject.pop(item)
+                dict_object[Province.XIZANG.value] = dict_object.pop(item)
             elif item in province.AOMEN.value:
-                dictObject[Province.AOMEN.value] = dictObject.pop(item)
-        return dictObject
+                dict_object[Province.AOMEN.value] = dict_object.pop(item)
+        return dict_object
 
 
 @unique
