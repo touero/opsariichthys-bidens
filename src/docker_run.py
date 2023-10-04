0e3ee0346d07e6375f36cc4c3f67ef8f708efbb4
1243569769@qq.com <1243569769@qq.com>
2023-10-04 12:49:20 +0800

    refactor：search something with docker

--- src/docker_run.py
+++ src/docker_run.py
@@ -1,71 +1,32 @@
-import docker
 import os
-
-from typing import Union
-from docker.errors import ImageNotFound
 from argparse import ArgumentParser
-from docker.models.containers import Container
-
-from src.tools import log_t
+from src.docker_re import DockerRe
 
 
-class DockerRun:
+class DockerRun(DockerRe):
     def __init__(self, yaml_dir: str):
+        super().__init__()
         self.yaml_dir = yaml_dir
-        self.client = docker.from_env()
         self.image_name = 'python:3.9'
         self.container_name = 'opsariichthys-bidens'
 
-    def get_image(self):
-        try:
-            self.client.images.get(self.image_name)
-        except ImageNotFound as e:
-            log_t(f'ImageNotFound: {str(e)}')
-            log_t(f'waiting docker pull {self.image_name}')
-            self.client.images.pull(self.image_name)
-            log_t(f'docker pull {self.image_name} finish')
-        except Exception as e:
-            log_t(str(e))
-
-    def get_container(self) -> Union[Container, None]:
-        containers = self.client.containers.list(all=True)
-        for container in containers:
-            if self.container_name == container.name:
-                log_t(f'docker find container: {container.id}')
-                return container
-        return None
-
-    def run_container(self):
+    def config(self):
         host_script = os.path.dirname(os.getcwd())
-        container_script = "/path/to/container"
-        log_t(host_script, container_script)
-        volumes = {
-            host_script: {
-                "bind": container_script,
-                "mode": "rw"
-            }
+        container_script = '/path/to/container'
+        return {
+            'image': self.image_name,
+            'name': self.container_name,
+            'ports': {'2518/tcp': 2518},
+            'volumes': {
+                f'{host_script}': {'bind': container_script, 'mode': 'rw'}
+            },
+            'detach': True,
+            'command': ["sh", "-c", f'cd {container_script} &&'
+                                    'pip install --root-user-action=ignore requests &&'
+                                    'pip install -r requirements.txt &&'
+                                    'cd src &&'
+                                    f'python config_run.py -y {self.yaml_dir}']
         }
-        container = self.client.containers.run(
-            self.image_name,
-            detach=True,
-            volumes=volumes,
-            name=self.container_name,
-            command=["sh", "-c", f'cd {container_script} &&'
-                                 'pip install --root-user-action=ignore requests &&'
-                                 'pip install -r requirements.txt &&'
-                                 'cd src &&'
-                                 f'python config_run.py -y {self.yaml_dir}'],
-            ports={'2518/tcp': 2518}
-        )
-        log_t(f'container id: {container.id} is running')
-
-    def start(self):
-        self.get_image()
-        container = self.get_container()
-        if container:
-            container.start()
-        else:
-            self.run_container()
 
 
 parser = ArgumentParser()
