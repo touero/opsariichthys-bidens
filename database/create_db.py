6254d2f0d315491b7ab640ffd2a04f93459a7478
touero <touer0018@gmail.com>
2024-09-03 09:29:32 +0800

    fix: create database without command line parameters

--- database/create_db.py
+++ database/create_db.py
@@ -1,22 +1,23 @@
-import os
 import sqlite3
 import traceback
 
+from pathlib import Path
 from logs import log
 
 
 def create_db():
-    log('checking if the sqlite database already exists')
-    if os.path.exists('database/all_school.db'):
-        log('sqlite database already exists, skipping creation')
+    db_path = Path('database/all_school.db')
+    sqlite_path = Path('database/all_school_sqlite.sql')
+    if db_path.exists():
+        log('Checking sqlite database already exists, skipping creation.')
         return
 
-    log('creating the sqlite database')
-    connection = sqlite3.connect('database/all_school.db')
-    with open('database/all_school_sqlite.sql', 'r', encoding='utf-8') as f:
+    log(f'Reading the {sqlite_path} to creating the sqlite database.')
+    connection = sqlite3.connect(db_path)
+    with open(sqlite_path, 'r', encoding='utf-8') as f:
         sql_commands = f.read()
     try:
-        log('waiting database to be created')
+        log('Waiting database to be created.')
         cursor = connection.cursor()
         cursor.executescript(sql_commands)
     except Exception as e:
@@ -25,4 +26,4 @@ def create_db():
     finally:
         connection.commit()
         connection.close()
-        log('creating the database finish')
+        log(f'Creating the database successfully in {db_path}.')
